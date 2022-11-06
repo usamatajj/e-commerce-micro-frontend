0efb3b3cbf27b355d3f48e7ad8d0490de97b438d
usamatajj <usama.taj@hotmail.com>
2022-11-06 23:40:23 +0500

    container changes

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi There! I Am Marketing</h1>
+      <h1>Hi There! I Am Container</h1>
       <hr />
       <MarketingApp />
     </div>
