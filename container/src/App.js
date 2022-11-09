e5a990614d7902db9275d7884f9c088d9bd4eb3c
usamatajj <usama.taj@hotmail.com>
2022-11-09 11:48:59 +0500

    fixing css class conflict issue

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,23 @@
+// NPM
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
+
+// User Defined
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
